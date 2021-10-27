a9fed87ed5ad0e0420be4c5ceb1f845ecc2c1862
srokks <srokks@gmail.com>
2021-10-27 18:22:21 +0200

    .

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,7 +1,13 @@
 # Write your code here
+import string
+
 from lxml import etree
 from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
+
+
 def parse_from_xml():
     file_name = 'news.xml'
     root = etree.parse(file_name).getroot()
@@ -11,48 +17,46 @@ def parse_from_xml():
             if elle.get('name') == 'head':
                 head = elle.text
             if elle.get('name') == 'text':
-                tail = elle.text
-        parsed_text_dict.update({head:{'tail':tail}})
+                tail = elle.text.lower()
+        parsed_text_dict.update({head: {'tail': tail}})
     return parsed_text_dict
 
-def gen_tokens(text):
-    return sorted(word_tokenize(text.lower()),reverse=True)
-
-def gen_most_freq_words(parsed_text_dict):
-    for head,tail in parsed_text_dict.items():
-        tokens = gen_tokens(tail)
-        parsed_text_dict[head] = Counter(tokens).most_common(5)
 
 def tokenize(words_dict):
     for head in words_dict:
-        words_dict[head].update({'tokens':word_tokenize(words_dict[head]['tail'])})
+        words_dict[head].update({'tokens':sorted(word_tokenize(words_dict[head]['tail']),reverse=True)})
+
+
 def lemmatize(words_dict):
     lemm = WordNetLemmatizer()
     for head in words_dict:
-        words_dict[head].update({'lemms':[lemm.lemmatize(x) for x  in words_dict[head]['tokens']]})
+        words_dict[head].update({'lemms': [lemm.lemmatize(x) for x in words_dict[head]['tokens']]})
+
+
 def remove_not_words(words_dict):
     not_allowed_list = stopwords.words('english')
     not_allowed_list.extend(string.punctuation)
     for head in words_dict:
         temp_list = [item for item in words_dict[head]['lemms'] if item not in not_allowed_list]
         words_dict[head]['lemms'] = temp_list
-    # print(stopwords.words('english'))
-    # print(list(string.punctuation))
-    #removes punctuations
-    #print([item for item in a if item not in list(string.punctuation)])
+
+def most_freq(words_dict):
+    for head in words_dict:
+        words_dict[head].update({'most_freq':Counter(words_dict[head]['lemms']).most_common(5)})
 def print_most_frex_words(words_dict):
     for head in words_dict:
         print(f'{head}:')
-        print(words_dict[head]['lemms'])
-        # print(' '.join(([x for x,y in tail])))
+        # print(words_dict[head]['most_freq'])
+        print(' '.join(([x for x,y in words_dict[head]['most_freq']])))
         # print(sorted(tail.items(),key= lambda item: item[1],reverse=True))
         # for el in tail[]:
         #     print(el)
 
-# counter = Counter(gen_tokens(test_txt))
+
 parsed_text_dict = parse_from_xml()
 tokenize(parsed_text_dict)
 lemmatize(parsed_text_dict)
 remove_not_words(parsed_text_dict)
+most_freq(parsed_text_dict)
 print_most_frex_words(parsed_text_dict)
 pass
